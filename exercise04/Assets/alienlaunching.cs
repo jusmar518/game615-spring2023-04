fb09e9441e0cc865d6d6fb7b85ae274228c42bcf
tighnarifox <123525841+tighnarifox@users.noreply.github.com>
2023-02-15 10:38:39 -0500

    ias

---
+++ exercise04/Assets/alienlaunching.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alienlaunching : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject alien = Instantiate(alien, gameObject.transform.position, Quaternion.identity);
+                float rotXAmount = Random.Range(-89, -10);
+                float rotYAmount = Random.Range(0, 360);
+                alien.transform.Rotate(rotXAmount, rotYAmount, 0);
+                Rigidbody rb = alien.GetComponent<Rigidbody>();
+                rb.AddForce(alien.transform.forward * 1000);
+
+                Destroy(alien, 2f);
+            }
+        
+    }
+}
