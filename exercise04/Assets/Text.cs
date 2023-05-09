2c4d807937d455223362ff47506652ff8b342a53
jusmar518 <jm8612b@student.american.edu>
2023-05-09 17:59:53 -0400

    changes

--- exercise04/Assets/Text.cs
+++ exercise04/Assets/Text.cs
@@ -1,69 +1,26 @@
+using System;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
+public class CountdownTimer : MonoBehaviour
 {
-    public float timeLimit = 60.0f; // Set the time limit to 60 seconds
-    public GameObject player; // Reference to the player object
-    public GameObject[] collectibles; // Array of all collectible objects
+    public Text countdownText;
+    private float currentTime = 60f;
+    
 
-    private float timeRemaining;
-    private Text timerText;
-
-    void Start()
+    private void Update()
     {
-        timeRemaining = timeLimit;
-        timerText = GetComponent<Text>();
-    }
+        currentTime -= Time.deltaTime;
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        countdownText.text = seconds.ToString();
 
-    void Update()
-    {
-        if (timeRemaining > 0.0f)
+        if (currentTime <= 0)
         {
-            timeRemaining -= Time.deltaTime;
-            UpdateTimerText();
+            currentTime = 0;
+            Debug.Log("Time's up!");
         }
-        else
-        {
-            Destroy(player);
-            DisplayLoseMessage();
-        }
-
-        if (AllCollectiblesCollected())
-        {
-            Destroy(player);
-            DisplayWinMessage();
-        }
-    }
-
-    bool AllCollectiblesCollected()
-    {
-        foreach (GameObject collectible in collectibles)
-        {
-            if (collectible.activeInHierarchy)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    void UpdateTimerText()
-    {
-        int minutes = Mathf.FloorToInt(timeRemaining / 60.0f);
-        int seconds = Mathf.FloorToInt(timeRemaining % 60.0f);
-        string timeString = string.Format("{0:00}:{1:00}", minutes, seconds);
-        timerText.text = "Time: " + timeString;
-    }
-
-    void DisplayWinMessage()
-    {
-        Debug.Log("You win! :)");
-    }
-
-    void DisplayLoseMessage()
-    {
-        Debug.Log("You lose. :(");
     }
 }
+
 
