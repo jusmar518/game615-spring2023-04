f8f0f60f18fd54cb2a5b2324b9089027d1f38635
jusmar518 <jm8612b@student.american.edu>
2023-05-10 17:45:33 -0400

    push

---
+++ exercise04/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,241 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void alienlaunching::Update()
+extern void alienlaunching_Update_mF02685E534A5BB3F2A28051AEF90021061F1EF81 (void);
+// 0x00000002 System.Void alienlaunching::.ctor()
+extern void alienlaunching__ctor_m24D07B796EF6AAAF183533EB6B84CCD24125B55F (void);
+// 0x00000003 System.Void alienScore::Start()
+extern void alienScore_Start_m2E098984FCBE3072D404D7613CF00F2BCA738B87 (void);
+// 0x00000004 System.Void alienScore::Update()
+extern void alienScore_Update_m85B842D5CDC2A6C3581832308135ABA045AC72B7 (void);
+// 0x00000005 System.Void alienScore::OnTriggerEnter(UnityEngine.Collider)
+extern void alienScore_OnTriggerEnter_m37AB74989B73B5D1A620055E98C2B7DACD4013F7 (void);
+// 0x00000006 System.Void alienScore::.ctor()
+extern void alienScore__ctor_mED09EF23EAAA029D25DB7BF030EEE7C9A6D6467A (void);
+// 0x00000007 System.Void alienTimer::Start()
+extern void alienTimer_Start_m9640F0AE4B50EB4B53E4F0D2E13A5435E3E96304 (void);
+// 0x00000008 System.Void alienTimer::Update()
+extern void alienTimer_Update_m56EDEAFDA0F258BD290366ECC83963B96088C563 (void);
+// 0x00000009 System.Void alienTimer::OnTriggerEnter(UnityEngine.Collider)
+extern void alienTimer_OnTriggerEnter_mC3EF542FD3D5F0570C1160E736ADAC665EEBA4B7 (void);
+// 0x0000000A System.Void alienTimer::.ctor()
+extern void alienTimer__ctor_m24DB4D37AAF86C905F2BE81327E7D4FDCCD689CC (void);
+// 0x0000000B System.Void Collectible::Start()
+extern void Collectible_Start_m56F79FEAEDB6C527A839441BC162207840C16603 (void);
+// 0x0000000C System.Void Collectible::Update()
+extern void Collectible_Update_mD03AD2C027908F9A753017FB1FE8E5044F236DCE (void);
+// 0x0000000D System.Void Collectible::OnTriggerEnter(UnityEngine.Collider)
+extern void Collectible_OnTriggerEnter_m256528F5206DDF59FE1DDA30BDA5DF44D5306C69 (void);
+// 0x0000000E System.Void Collectible::.ctor()
+extern void Collectible__ctor_mD2FAE521B61DCEEA10D4B227E5F2F3467260F950 (void);
+// 0x0000000F System.Void mover::Start()
+extern void mover_Start_m475D166CBC3B66EA9E1A5383307C76473F00AE0E (void);
+// 0x00000010 System.Void mover::Update()
+extern void mover_Update_m4263C7CBA21353DE7F891EA43CC389BFC05E9DE8 (void);
+// 0x00000011 System.Void mover::Moving()
+extern void mover_Moving_m300D2BCA7BB85B8E1A8882BCFCE16B2C221E2FFB (void);
+// 0x00000012 System.Void mover::.ctor()
+extern void mover__ctor_m78179291BC179BDFBB1BEFE746F6157B252D4B8A (void);
+// 0x00000013 System.Void PlayerControls::Start()
+extern void PlayerControls_Start_m31F1265A18F322E8BCD0379BA4ECB57479776660 (void);
+// 0x00000014 System.Void PlayerControls::Update()
+extern void PlayerControls_Update_m1FB8C4FBC7768E727B22A107B203C853DBECCC44 (void);
+// 0x00000015 System.Void PlayerControls::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerControls_OnTriggerEnter_m68D230EDC18D40A7A5405D3A1DE7985423AB3B02 (void);
+// 0x00000016 System.Void PlayerControls::.ctor()
+extern void PlayerControls__ctor_m332F79A4E651845074B3ABFAEF8F6F3604244309 (void);
+// 0x00000017 System.Void scoreboard::Start()
+extern void scoreboard_Start_m84E39BC4D4DE36F034D1F8E4EEE4A4A9D2969E32 (void);
+// 0x00000018 System.Void scoreboard::Update()
+extern void scoreboard_Update_m5DDC75A28B234AB73B737104419CB7DE7F87A458 (void);
+// 0x00000019 System.Void scoreboard::IncreaseScore(System.Int32)
+extern void scoreboard_IncreaseScore_mF8848211F09A2B1C6E4CDD16D2781FC5B4F9244F (void);
+// 0x0000001A System.Void scoreboard::.ctor()
+extern void scoreboard__ctor_m4604E8285A85EAD39A554C4CA01D72425F5207E0 (void);
+// 0x0000001B System.Void ScoreManager::Awake()
+extern void ScoreManager_Awake_mB3BB0D0448D4C240C4572B7395AF328615E76D8A (void);
+// 0x0000001C System.Void ScoreManager::Start()
+extern void ScoreManager_Start_m7E00370AD0134DB3AEA647B9BA69055725AA83FE (void);
+// 0x0000001D System.Void ScoreManager::AddPoint()
+extern void ScoreManager_AddPoint_m2B412DED79C7DC2468E41A418BDEA31E760D03ED (void);
+// 0x0000001E System.Void ScoreManager::.ctor()
+extern void ScoreManager__ctor_mE5C264C34A9CBBAB72DCF99CCCC9841AF8F9FEE6 (void);
+// 0x0000001F System.Void ScoringSystem::AddPoints(System.Int32)
+extern void ScoringSystem_AddPoints_m58146042F1E23057306C642CEECCA36B9DE43322 (void);
+// 0x00000020 System.Void ScoringSystem::ResetScore()
+extern void ScoringSystem_ResetScore_m65495A7F4874D62A13CA101705A95656EEB32A3A (void);
+// 0x00000021 System.Void ScoringSystem::Start()
+extern void ScoringSystem_Start_mD3B43AC1F85AF7CD1021008587C8B2708678B589 (void);
+// 0x00000022 System.Void ScoringSystem::OnDestroy()
+extern void ScoringSystem_OnDestroy_mC7AE6A002BFFAFF4D169F8A8709E64F02E140020 (void);
+// 0x00000023 System.Void ScoringSystem::Awake()
+extern void ScoringSystem_Awake_mDC35222C18948AC63D9036906C1BF66A45790701 (void);
+// 0x00000024 System.Void ScoringSystem::.ctor()
+extern void ScoringSystem__ctor_m53AA42C2FD4BA79D53A51BA1E6A158B04439B2DF (void);
+// 0x00000025 System.Void LookCamera::Start()
+extern void LookCamera_Start_mE9289E1E6CB37C5254B8BCAA97DEF20F5B8E3D7B (void);
+// 0x00000026 System.Void LookCamera::Update()
+extern void LookCamera_Update_m68E69B1B5A7045536596C77570DDBB111AC6DEE0 (void);
+// 0x00000027 System.Void LookCamera::.ctor()
+extern void LookCamera__ctor_m14F0C4DDA1FF55558C400791F6361D35EA975CB4 (void);
+// 0x00000028 System.Void CountdownTimer::Update()
+extern void CountdownTimer_Update_m09454B2CE79137FE22154455DEBC3FD4D9336690 (void);
+// 0x00000029 System.Void CountdownTimer::.ctor()
+extern void CountdownTimer__ctor_mD11E5A7608DA470F85740007D0FFB9B30F8E3F6E (void);
+// 0x0000002A System.Void Timer::Start()
+extern void Timer_Start_m6820D211A06B2E5E24795D7EBE67EE865F39BC43 (void);
+// 0x0000002B System.Void Timer::Update()
+extern void Timer_Update_m3AC30EFD54E8E1010F6E601AE179212E71E6B2CD (void);
+// 0x0000002C System.Void Timer::updateTimer(System.Single)
+extern void Timer_updateTimer_mBA913D114AD060FC574FEDFFD44BA747A849A5EA (void);
+// 0x0000002D System.Void Timer::.ctor()
+extern void Timer__ctor_m5FF13F1DAD0527F97E229A1904A8AD662731C4B5 (void);
+// 0x0000002E System.Void UI::.ctor()
+extern void UI__ctor_m177FCA8E7C6A148BFF6FED9F758B3396F25FDFBB (void);
+// 0x0000002F System.Void ventis::.ctor()
+extern void ventis__ctor_m0A66CEFC157CC9D35AA9FFE75758B8B501B67201 (void);
+// 0x00000030 System.Void WinCondition::Start()
+extern void WinCondition_Start_m4116972C97816BF15807635354DEF47321E4C763 (void);
+// 0x00000031 System.Void WinCondition::Update()
+extern void WinCondition_Update_mA2663AF38FEF440EED925A388FF0B800E4D9740D (void);
+// 0x00000032 System.Void WinCondition::.ctor()
+extern void WinCondition__ctor_m798FCB3FB39DDC66FC55693E80785F369EB935D0 (void);
+static Il2CppMethodPointer s_methodPointers[50] = 
+{
+	alienlaunching_Update_mF02685E534A5BB3F2A28051AEF90021061F1EF81,
+	alienlaunching__ctor_m24D07B796EF6AAAF183533EB6B84CCD24125B55F,
+	alienScore_Start_m2E098984FCBE3072D404D7613CF00F2BCA738B87,
+	alienScore_Update_m85B842D5CDC2A6C3581832308135ABA045AC72B7,
+	alienScore_OnTriggerEnter_m37AB74989B73B5D1A620055E98C2B7DACD4013F7,
+	alienScore__ctor_mED09EF23EAAA029D25DB7BF030EEE7C9A6D6467A,
+	alienTimer_Start_m9640F0AE4B50EB4B53E4F0D2E13A5435E3E96304,
+	alienTimer_Update_m56EDEAFDA0F258BD290366ECC83963B96088C563,
+	alienTimer_OnTriggerEnter_mC3EF542FD3D5F0570C1160E736ADAC665EEBA4B7,
+	alienTimer__ctor_m24DB4D37AAF86C905F2BE81327E7D4FDCCD689CC,
+	Collectible_Start_m56F79FEAEDB6C527A839441BC162207840C16603,
+	Collectible_Update_mD03AD2C027908F9A753017FB1FE8E5044F236DCE,
+	Collectible_OnTriggerEnter_m256528F5206DDF59FE1DDA30BDA5DF44D5306C69,
+	Collectible__ctor_mD2FAE521B61DCEEA10D4B227E5F2F3467260F950,
+	mover_Start_m475D166CBC3B66EA9E1A5383307C76473F00AE0E,
+	mover_Update_m4263C7CBA21353DE7F891EA43CC389BFC05E9DE8,
+	mover_Moving_m300D2BCA7BB85B8E1A8882BCFCE16B2C221E2FFB,
+	mover__ctor_m78179291BC179BDFBB1BEFE746F6157B252D4B8A,
+	PlayerControls_Start_m31F1265A18F322E8BCD0379BA4ECB57479776660,
+	PlayerControls_Update_m1FB8C4FBC7768E727B22A107B203C853DBECCC44,
+	PlayerControls_OnTriggerEnter_m68D230EDC18D40A7A5405D3A1DE7985423AB3B02,
+	PlayerControls__ctor_m332F79A4E651845074B3ABFAEF8F6F3604244309,
+	scoreboard_Start_m84E39BC4D4DE36F034D1F8E4EEE4A4A9D2969E32,
+	scoreboard_Update_m5DDC75A28B234AB73B737104419CB7DE7F87A458,
+	scoreboard_IncreaseScore_mF8848211F09A2B1C6E4CDD16D2781FC5B4F9244F,
+	scoreboard__ctor_m4604E8285A85EAD39A554C4CA01D72425F5207E0,
+	ScoreManager_Awake_mB3BB0D0448D4C240C4572B7395AF328615E76D8A,
+	ScoreManager_Start_m7E00370AD0134DB3AEA647B9BA69055725AA83FE,
+	ScoreManager_AddPoint_m2B412DED79C7DC2468E41A418BDEA31E760D03ED,
+	ScoreManager__ctor_mE5C264C34A9CBBAB72DCF99CCCC9841AF8F9FEE6,
+	ScoringSystem_AddPoints_m58146042F1E23057306C642CEECCA36B9DE43322,
+	ScoringSystem_ResetScore_m65495A7F4874D62A13CA101705A95656EEB32A3A,
+	ScoringSystem_Start_mD3B43AC1F85AF7CD1021008587C8B2708678B589,
+	ScoringSystem_OnDestroy_mC7AE6A002BFFAFF4D169F8A8709E64F02E140020,
+	ScoringSystem_Awake_mDC35222C18948AC63D9036906C1BF66A45790701,
+	ScoringSystem__ctor_m53AA42C2FD4BA79D53A51BA1E6A158B04439B2DF,
+	LookCamera_Start_mE9289E1E6CB37C5254B8BCAA97DEF20F5B8E3D7B,
+	LookCamera_Update_m68E69B1B5A7045536596C77570DDBB111AC6DEE0,
+	LookCamera__ctor_m14F0C4DDA1FF55558C400791F6361D35EA975CB4,
+	CountdownTimer_Update_m09454B2CE79137FE22154455DEBC3FD4D9336690,
+	CountdownTimer__ctor_mD11E5A7608DA470F85740007D0FFB9B30F8E3F6E,
+	Timer_Start_m6820D211A06B2E5E24795D7EBE67EE865F39BC43,
+	Timer_Update_m3AC30EFD54E8E1010F6E601AE179212E71E6B2CD,
+	Timer_updateTimer_mBA913D114AD060FC574FEDFFD44BA747A849A5EA,
+	Timer__ctor_m5FF13F1DAD0527F97E229A1904A8AD662731C4B5,
+	UI__ctor_m177FCA8E7C6A148BFF6FED9F758B3396F25FDFBB,
+	ventis__ctor_m0A66CEFC157CC9D35AA9FFE75758B8B501B67201,
+	WinCondition_Start_m4116972C97816BF15807635354DEF47321E4C763,
+	WinCondition_Update_mA2663AF38FEF440EED925A388FF0B800E4D9740D,
+	WinCondition__ctor_m798FCB3FB39DDC66FC55693E80785F369EB935D0,
+};
+static const int32_t s_InvokerIndices[50] = 
+{
+	3420,
+	3420,
+	3420,
+	3420,
+	2786,
+	3420,
+	3420,
+	3420,
+	2786,
+	3420,
+	3420,
+	3420,
+	2786,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	2786,
+	3420,
+	3420,
+	3420,
+	2768,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	2768,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	2817,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+	3420,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	50,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
